ca0fda37ca98c7ab1da9d8709ffd149ae047ace0
ynjacobs <ytj64611@gamil.com>
2019-06-18 13:42:49 -0400

    when switch is turned off or on color and text changes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 
 
+    // Let's add our first event handlers to this simple document
+        const lightOn = document.querySelector('.switch ');
+        const ChangeText = document.querySelector('.status ');
+
+        lightOn.addEventListener('click', function(e) {
+            if(e.target.classList.contains('on')) {
+                e.target.classList.remove('on');
+                e.target.classList.add('off');
+                document.body.className = 'dark';
+                ChangeText.innerHTML = "Hey, who turned off the lights?";
+              }
+            else if (e.target.classList.contains('off')) {
+                e.target.classList.remove('off'); 
+                e.target.classList.add('on');
+                document.body.className = 'light';
+                ChangeText.innerHTML = "It's so bright in here!";
+              }
+            });
+
+        
+       
+            
+    
+    
+
+
